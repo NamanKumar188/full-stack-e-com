ca9468f0d24a908d711e48879497dc02cd1190a0
Naman <namankumar1882002@gmail.com>
2024-08-10 13:30:08 +0530

    second

--- client/src/Components/buynow/Buynow.js
+++ client/src/Components/buynow/Buynow.js
@@ -12,7 +12,7 @@ const Buynow = () => {
     // console.log(cartdata.length);
 
     const getdatabuy = async () => {
-        const res = await fetch("/cartdetails", {
+        const res = await fetch("https://full-stack-e-com-1.onrender.com/cartdetails", {
             method: "GET",
             headers: {
                 Accept:"application/json",
